2365a4b0d0b4e735989e4ade1ddfcf183209550d
Dani Kalouche <tug39573@temple.edu>
2019-10-29 19:40:59 -0400

    MPI testing

--- mmult_mpi_omp.c
+++ mmult_mpi_omp.c
@@ -23,8 +23,11 @@ int main(int argc, char* argv[])
   double *bb;	/* the B matrix */
   double *cc1;	/* A x B computed using the omp-mpi code you write */
   double *cc2;	/* A x B computed using the conventional algorithm */
+  double *testCC;
   int myid, numprocs;
   double starttime, endtime;
+  int perRows, rowsLeft, offset;
+  
   MPI_Status status;
   /* insert other global variables here */
   MPI_Init(&argc, &argv);
@@ -39,12 +42,34 @@ int main(int argc, char* argv[])
       bb = gen_matrix(ncols, nrows);
       cc1 = malloc(sizeof(double) * nrows * nrows); 
       starttime = MPI_Wtime();
-	  int i;
+      
+      perRows = nrows / (numprocs-1);
+      rowsLeft = nrows / (numprocs-1);
+      offset = 0;
+      
+	  int i, j;
 	  for(i=1;i<numprocs;i++) {
 		  number *= i;
 		  printf("sending to %d\n", i);
-		  MPI_Send(&(aa[0]), 2*nrows*ncols, MPI_INT, i, 0, MPI_COMM_WORLD);
+		  MPI_Send(&(bb[0]), 2*nrows*ncols, MPI_INT, i, 0, MPI_COMM_WORLD);
+          MPI_Send(&offset,1, MPI_INT, i, 0, MPI_COMM_WORLD);
+          if(i== numprocs-1)
+            perRows += rowsLeft;
+          MPI_Send(&perRows,1, MPI_INT, i, 0, MPI_COMM_WORLD);
+          for (j=0; j< nrows*perRows; j++){
+            MPI_Send(&(aa[offset*ncols+j]),2,MPI_INT,i,0,MPI_COMM_WORLD);
+          }
+          MPI_Send(&(bb[offset*ncols+j]),2*ncols,MPI_INT,i,0,MPI_COMM_WORLD);
+          offset+=perRows;
 	  }
+      
+      testCC = malloc(sizeof(double)*2 *nrows*nrows);
+      for(i=1;i<numprocs;i++){
+        MPI_Recv(&offset,1,MPI_INT,i,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+        MPI_Recv(&(testCC[nrows*offset]),2*nrows*nrows,MPI_INT,i,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+      }
+      cc1 = testCC;
+      
       /* Insert your master code here to store the product into cc1 */
       endtime = MPI_Wtime();
       printf("%f\n",(endtime - starttime));
@@ -54,9 +79,22 @@ int main(int argc, char* argv[])
 
     } else {
       // Slave Code goes here
-	  aa = malloc(sizeof(double) * nrows * ncols);
-	  MPI_Recv(&(aa[0]), 2*nrows*ncols, MPI_INT, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-	  produceMatrix(aa, nrows, ncols);
+	  bb = malloc(sizeof(double) * nrows * ncols);
+	  MPI_Recv(&(bb[0]), 2*nrows*ncols, MPI_INT, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+      MPI_Recv(&offset,1,MPI_INT,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+      MPI_Recv(&perRows,1,MPI_INT,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+	  
+      aa = malloc(sizeof(double)*ncols*perRows);
+      testCC = malloc(sizeof(double)*nrows*nrows);
+      
+      in m,l;
+      for(m=0; m<ncols*perRows;m++){
+        MPI_Recv(&(aa[m]),2,MPI_INT,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+      }
+      
+      mmult(testCC,aa,perRows,ncols,bb,nrows,ncols);
+      MPI_Send(&offset,1,MPI_INT,0,0,MPI_COMM_WORLD);
+      MPI_Send(&(testCC[0]),2*nrows*nrows,MPI_INT,0,0,MPI_COMM_WORLD);
     }
   } else {
     fprintf(stderr, "Usage matrix_times_vector <size>\n");
